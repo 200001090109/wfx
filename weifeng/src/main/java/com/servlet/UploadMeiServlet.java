8662247c13eaa3c348c87b011068308a900e49a7
CZwell <578335866@qq.com>
2021-12-07 23:23:56 +0800

    上传图片-未完成

--- weifeng/src/main/java/com/servlet/UploadMeiServlet.java
+++ weifeng/src/main/java/com/servlet/UploadMeiServlet.java
@@ -3,6 +3,9 @@ package com.servlet;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+
+import com.service.imp.BusinessServiceImp;
+import com.utils.Upload;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
@@ -16,6 +19,10 @@ public class UploadMeiServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String filePath = Upload.upload(request, response);
+        String[] webapps = filePath.split("\\\\");
+        filePath = webapps[webapps.length-2] + "/" + webapps[webapps.length-1];
+        BusinessServiceImp bs = new BusinessServiceImp();
 
     }
 }
