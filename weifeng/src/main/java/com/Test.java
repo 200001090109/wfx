f2effeaf29d31e93c438971fd69c04366aa21cd2
cq128 <3206293873@qq.com>
2021-12-07 15:51:15 +0800

    前端点赞收藏显示绑定

--- weifeng/src/main/java/com/Test.java
+++ weifeng/src/main/java/com/Test.java
@@ -1,13 +1,19 @@
 package com;
 
-import com.utils.CodeImageUtil;
+import com.model.Wmei;
+import com.service.imp.BusinessServiceImp;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        try{
-            CodeImageUtil.getCode("E:\\Projects\\wfx\\weifeng\\src\\main\\webapp\\images\\",1);
-        }catch (Exception e){
-            e.printStackTrace();
+        BusinessServiceImp bs = new BusinessServiceImp();
+        List<Wmei> meis = bs.getAllZanById(1);
+        ArrayList<Long> zans = new ArrayList();
+        for(Wmei mei:meis){
+            zans.add(mei.getMie().getId());
         }
+        System.out.println(zans.contains(Long.parseLong("3")));
     }
 }
